ec008eaeb5410e0dba3dd27a05e121f0bc48420b
simonsousa <simonsousavelarde@gmail.com>
2025-05-09 11:03:19 -0500

    cambio al logo y navbar

--- app/components/Navegacion.tsx
+++ app/components/Navegacion.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Navegacion() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -14,14 +15,19 @@ export default function Navegacion() {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-darkgreen text-white shadow-lg font-poppins">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-beige text-darkgreen shadow-lg font-poppins">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-4 sm:px-6">
-        {/* Logo (temporal: solo texto) */}
-        <Link
-          href="/"
-          className="text-xl font-bold tracking-wide text-white hover:text-lightmustard transition-colors duration-200"
-        >
-          Antigua Bodega
+        {/* Logo */}
+
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/LogoDalmacia/dalmacia.png"
+            alt="Logo BodegaD"
+            width={140} // puedes ajustar según se necesite
+            height={70}
+            className="h-auto w-auto"
+            priority
+          />
         </Link>
 
         {/* Menú desktop */}
@@ -40,7 +46,7 @@ export default function Navegacion() {
 
         {/* Botón menú móvil */}
         <button
-          className="md:hidden text-white focus:outline-none"
+          className="md:hidden text-darkgreen focus:outline-none"
           onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
           aria-label="Abrir menú"
         >
@@ -72,8 +78,8 @@ export default function Navegacion() {
 
       {/* Menú móvil */}
       {mobileMenuOpen && (
-        <div className="md:hidden bg-darkgreen px-6 pt-4 pb-6 shadow-lg">
-          <ul className="flex flex-col gap-4 font-medium text-white">
+        <div className="md:hidden bg-beige px-6 pt-4 pb-6 shadow-lg">
+          <ul className="flex flex-col gap-4 font-medium text-darkgreen">
             {navItems.map((item) => (
               <li key={item.name}>
                 <a
