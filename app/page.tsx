fb5a0fbd9ecc446ef2e21b9e444b963c14d65054
simonsousa <simonsousavelarde@gmail.com>
2025-12-30 11:43:11 -0500

    carta navideñ REMOVED

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Hero />
       <Nosotros />
       <CarouselPlatos />
-      <CartaNavidena />
+      {/* <CartaNavidena /> */}
       <Cartas />
       <SocialProof />
       <Reservas />
